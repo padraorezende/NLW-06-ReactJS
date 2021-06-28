f641747393ab7c7daee06022db6723bcdf67af71
padraorezende <padraonxgalo@gmail.com>
2021-06-28 14:28:35 -0300

    Criando projeto e definindo as variaveis de ambiente

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/button';
+
+function App() {
+  return (
+    <div>
+      <Button text="logar" />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
